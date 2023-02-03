f4d832bb18054acbd1f7480281d215e2c3e3ffde
XiaoMaShawn <ma2014xiao@hotmail.com>
2023-02-03 10:44:06 -0500

    snake draw function and logic

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+// const SNAKE_SPEED = 2;
+const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondSinceLastRender < 1 / SNAKE_SPEED) return
-  console.log("Render");
+  // console.log("Render");
   lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(main);
+
+//update the snake and food
+function update() {
+  updateSnake();
+}
+
+//draw the snake and food
+function draw() {
+  drawSnake(gameBoard);
+}
