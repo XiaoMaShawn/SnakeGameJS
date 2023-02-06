db3fb2ce612ea0107dd5404dc6c9c3dc428694eb
XiaoMaShawn <ma2014xiao@hotmail.com>
2023-02-06 11:21:16 -0500

    snake control logic by arrow keys

--- snake.js
+++ snake.js
@@ -1,12 +1,15 @@
-export const SNAKE_SPEED = 1;
-const snakeBody = [{ x: 11, y: 11 }, { x: 12, y: 11 }, { x: 13, y: 11 }]
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }]
 
 export function update() {
+  const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
   // console.log('update snake');
 }
 
